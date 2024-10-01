347e46eae4af46f00dbcf40b3d8b86cdab2e6f78
gsreddy1870 <sudhakarareddy.gudipati@developtrees.com>
2024-10-01 14:26:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
-  region = "eu-north-1"
+AWS_Access_Key_ID: "AKIA3FLD3AQ4TLAYVKN3"
+AWS_Secret_Access_Key: "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"
+region = "eu-north-1"
 }
 resource "aws_instance" "name" {
   ami                    = "ami-04cdc91e49cb06165"
